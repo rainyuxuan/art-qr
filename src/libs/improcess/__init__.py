2a6b56329baa409f210e45ecf4be8415b858d3e2
rainyuxuan <yu2001xuan@outlook.com>
2022-06-11 17:01:01 -0400

    🚀 Added preprocessing

--- src/libs/improcess/__init__.py
+++ src/libs/improcess/__init__.py
@@ -5,6 +5,12 @@ import src.libs.ocr as ocr
 import src.libs.encryptor as enc
 
 
+class ColorNotFoundException(NotImplementedError):
+    def __init__(self, message='Color type not found'):
+        self.message = message
+        super().__init__(self.message)
+
+
 class ImageBase:
     path: str
     origin: np.ndarray
@@ -15,7 +21,7 @@ class ImageBase:
         self.path = path
         self.origin = cv2.imread(path, flag or cv2.IMREAD_UNCHANGED)
         self.data = cv2.cvtColor(self.origin, cv2.COLOR_BGR2RGB)
-        self.data_type = 'rgb'
+        self.data_type = 'RGB'
 
     def get_origin(self):
         return self.origin
@@ -27,19 +33,57 @@ class ImageBase:
         self.data = data
 
     def get_rgb(self):
-        return self.origin
+        if self.data_type == 'RGB':
+            return self.data
+        elif self.data_type == 'GRAY':
+            return cv2.cvtColor(self.data, cv2.COLOR_GRAY2RGB)
+        elif self.data_type == 'BGR':
+            return cv2.cvtColor(self.data, cv2.COLOR_BGR2RGB)
+        else:
+            raise ColorNotFoundException()
+
+    def get_bgr(self):
+        if self.data_type == 'BGR':
+            return self.data
+        elif self.data_type == 'GRAY':
+            return cv2.cvtColor(self.data, cv2.COLOR_GRAY2BGR)
+        elif self.data_type == 'RGB':
+            return cv2.cvtColor(self.data, cv2.COLOR_RGB2BGR)
+        else:
+            raise ColorNotFoundException()
+
+    def get_grayscale(self):
+        if self.data_type == 'GRAY':
+            return self.data
+        elif self.data_type == 'RGB':
+            return cv2.cvtColor(self.data, cv2.COLOR_RGB2GRAY)
+        elif self.data_type == 'BGR':
+            return cv2.cvtColor(self.data, cv2.COLOR_BGR2GRAY)
+        else:
+            raise ColorNotFoundException()
 
     def to_grayscale(self):
-        self.data = cv2.cvtColor(self.data, cv2.COLOR_RGB2GRAY)
+        if self.data_type == 'RGB':
+            self.data = cv2.cvtColor(self.data, cv2.COLOR_RGB2GRAY)
+        elif self.data_type == 'BGR':
+            self.data = cv2.cvtColor(self.data, cv2.COLOR_BGR2GRAY)
+        elif self.data_type == 'GRAY':
+            pass
+        else:
+            raise ColorNotFoundException()
+
+        self.data_type = 'GRAY'
         return self.data
 
 
 class SourceImage(ImageBase):
     def encrypt(self, secret) -> np.ndarray:
-        return enc.encrypt(self.data, secret)
+        self.data = enc.encrypt(self.data, secret)
+        return self.data
 
     def decrypt(self, key) -> np.ndarray:
-        return enc.decrypt(self.data, key)
+        self.data = enc.decrypt(self.data, key)
+        return self.data
 
 
 class TextImage(ImageBase):
@@ -49,13 +93,27 @@ class TextImage(ImageBase):
         super().__init__(path, flag)
         self.to_grayscale()
 
+    def get_text(self):
+        return self.text
+
     def set_text(self, text):
         self.text = text
 
     def preprocess(self, *flags) -> None:
-        self.data = ocr.preprocess(self.data, flags)
+        self.data = ocr.preprocess(self.data, *flags)
 
-    def to_text(self, *flags) -> str:
-        self.preprocess(flags)
-        return ocr.image_to_string(self.data)
+    def postprocess(self, *flags) -> None:
+        self.text = ocr.postprocess(self.text, *flags)
 
+    def to_text(self, **flags) -> str:
+        print("> OCR with flags:", flags)
+
+        if flags and flags['preprocess']:
+            self.preprocess(*flags['preprocess'])
+
+        self.text = ocr.image_to_string(self.data)
+
+        if 'postprocess' in flags:
+            self.postprocess(*flags['postprocess'])
+
+        return self.text
--- src/libs/ocr/__init__.py
+++ src/libs/ocr/__init__.py
@@ -6,11 +6,22 @@ import os
 
 
 def preprocess(image_data: np.ndarray, *flags):
-    return image_data
+    print('> Preprocess with flags', flags)
+    processed_image = image_data
+    if 'threshold' in flags:
+        processed_image = cv2.threshold(image_data, 0, 255,
+                                        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    if 'blur' in flags:
+        processed_image = cv2.medianBlur(processed_image, 3)
+
+    return processed_image
 
 
-def postprocess(text: str):
-    pass
+def postprocess(text: str, *flags):
+    print('> Postprocess with flags', flags)
+    processed_text = text
+
+    return processed_text
 
 
 def image_to_string(image_data: np.ndarray):
